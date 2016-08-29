8378e03c4f007c916291602a9ff50a3cacddbdc4
Jacliu17 <jackliu.mel@gmail.com>
2016-08-29 19:52:06 +1000

    Initial Commit

---
+++ VisitGuiLin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisitGuiLin
+//
+//  Created by Jack Liu on 29/08/2016.
+//  Copyright © 2016 com.jackliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
